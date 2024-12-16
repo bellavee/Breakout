29cef3349bbb616ce4c711f4c2c83f461c7291d8
Vy Vu <bella.vnpv@gmail.com>
2024-12-16 08:26:24 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    auto window = sf::RenderWindow({1280u, 720u}, "CMake SFML Project");
     window.setFramerateLimit(144);
 
     while (window.isOpen()) {
